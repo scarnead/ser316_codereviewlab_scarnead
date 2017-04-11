2dacd9b6c467c06dbe76b037277a2d9b7f9ce9b2
scarnead <scarnead@asu.edu>
2017-04-10 18:38:07 -0700

    bug-2 complete. Defect# 2resolved by adding descriptive names to the panel variables in MainFrame.java

--- src/banking/gui/MainFrame.java
+++ src/banking/gui/MainFrame.java
@@ -76,33 +76,33 @@ class MainFrame extends JFrame {
 		Container pane = getContentPane();
 		pane.setLayout(new FlowLayout());
 		
-		JPanel panel1 = new JPanel();
-		panel1.add(typeLabel);
-		panel1.add(typeOptions);
+		JPanel accountTypePane1 = new JPanel();
+		accountTypePane1.add(typeLabel);
+		accountTypePane1.add(typeOptions);
 		
-		JPanel panel2 = new JPanel();
-		panel2.add(displayAccountsButton);
-		panel2.add(displayAllAccountsButton);
-		panel2.add(saveButton);
+		JPanel displayButtonsPanel = new JPanel();
+		displayButtonsPanel.add(displayAccountsButton);
+		displayButtonsPanel.add(displayAllAccountsButton);
+		displayButtonsPanel.add(saveButton);
 		
-		JPanel panel3 = new JPanel();
-		panel3.add(nameLabel);
-		panel3.add(nameField);
+		JPanel namePanel = new JPanel();
+		namePanel.add(nameLabel);
+		namePanel.add(nameField);
 		
-		JPanel panel4 = new JPanel();
-		panel4.add(balanceLabel);
-		panel4.add(balanceField);
+		JPanel balancePanel = new JPanel();
+		balancePanel.add(balanceLabel);
+		balancePanel.add(balanceField);
 		
-		JPanel panel5 = new JPanel();
-		panel5.add(newAccountButton);
-		panel5.add(depositButton);
-		panel5.add(withdrawButton);
+		JPanel accountButtonsPanel = new JPanel();
+		accountButtonsPanel.add(newAccountButton);
+		accountButtonsPanel.add(depositButton);
+		accountButtonsPanel.add(withdrawButton);
 
-		pane.add(panel1);
-		pane.add(panel2);
-		pane.add(panel3);
-		pane.add(panel4);
-		pane.add(panel5);
+		pane.add(accountTypePane1);
+		pane.add(displayButtonsPanel);
+		pane.add(namePanel);
+		pane.add(balancePanel);
+		pane.add(accountButtonsPanel);
 		
 		setSize(400, 250);
 	}
